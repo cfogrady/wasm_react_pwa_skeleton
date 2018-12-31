add46c34f7a26e5550c6a2864d2ece84c826b3fd
Christopher O'Grady <cfogrady@gmail.com>
2018-12-31 21:26:09 +0000

    Add docs for github pages

---
+++ docs/precache-manifest.3aa52fad6f56390902dcbb0a32ab3076.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/wasm_react_pwa_skeleton/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "1f2e819d54ae9b02c122",
+    "url": "/wasm_react_pwa_skeleton/static/js/runtime~main.1f2e819d.js"
+  },
+  {
+    "revision": "7e406382496cd6406dc6",
+    "url": "/wasm_react_pwa_skeleton/static/js/main.7e406382.chunk.js"
+  },
+  {
+    "revision": "5fb161e1637aa45e9c43",
+    "url": "/wasm_react_pwa_skeleton/static/js/1.5fb161e1.chunk.js"
+  },
+  {
+    "revision": "7e406382496cd6406dc6",
+    "url": "/wasm_react_pwa_skeleton/static/css/main.8e6994a2.chunk.css"
+  },
+  {
+    "revision": "7544b592d5b13340b9060f048b54b44a",
+    "url": "/wasm_react_pwa_skeleton/index.html"
+  }
+];
